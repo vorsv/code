094ee7ac32a10da009e255a500d451b400036d7e
vorsv <66461662+vorsv@users.noreply.github.com>
2024-09-04 05:18:37 +0530

    haccer rnk

---
+++ hackker/kangaroo.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+class Solution{
+    public:
+    Solution(){
+        std::cout<<kangaroo(2,1,1,2);
+    }
+
+    std::string kangaroo(int x1, int v1, int x2, int v2) {
+        int pos1=0,pos2=2;
+        while(pos1!=pos2){
+            
+        }
+        return pos1==pos2? "YES" : "NO";
+    }
+
+};
+
+int main(){
+    Solution solution;
+}
